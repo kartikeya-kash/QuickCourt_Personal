a40726d796b4509f4af4c9117aa69a611b7a6a7b
Kartikeya <kartikeya.anjul@gmail.com>
2025-08-16 13:30:33 +0530

    navigation setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import React from "react";
-import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 
 function App() {
-
-return (
-  <div>
-    <Home/>
-  </div>
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
-
 }
 
 export default App;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { useState } from "react";
+import navigate from "react-router-dom";
 
 function Home() {
+  const navigate = useNavigate();
 
 return (
   <div>
+    <button onClick={()=>navigate("/Home")}>Home</button>
+    <button>Facility</button>
+    <button>My bookings</button>
+    <button>Me</button>
     <h1>Welcome to the Home Page</h1>
     <p>This is the home page of our application.</p>
   </div>
