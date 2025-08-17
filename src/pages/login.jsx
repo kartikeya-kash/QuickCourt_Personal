d8c225b19f9d292c05c5659c955bd352839d9c59
Kartikeya <kartikeya.anjul@gmail.com>
2025-08-17 14:04:37 +0530

    setup up login

---
+++ src/pages/login.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function Login() {
+const [username, setUsername] = useState("");
+const [password, setPassword] = useState("");
+const navigate = useNavigate();
+
+
+const handellogin = (e) => {
+    e.preventDefault();
+    window.alert("Login button clicked");
+    navigate("/"); // Redirect to home page after login
+};
+
+  return (
+    <div>
+      <h1>Login Page</h1>
+      
+      <form onSubmit={handellogin}>
+        <label>
+          Username:
+          <input type="text" name="username" value={username} onChange={(e)=> setUsername(e.target.value)}/>
+        </label>
+        <br />
+        <label>
+          Password:
+          <input type="password" name="password" value={password} onChange={(e)=> setPassword(e.target.value)}/>
+        </label>
+        <br />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
