a77ac5c4a62911cda2ccc11121e501f308e78c18
Kartikeya <kartikeya.anjul@gmail.com>
2025-08-21 21:07:54 +0530

    features of owner added

--- src/pages/owner.jsx
+++ src/pages/owner.jsx
@@ -5,7 +5,18 @@ import { useState } from "react";
 const Owner = () => {
   return (
     <div>
-      
+      <h1>Owner Dashboard</h1>
+
+
+      <p>Total users = <span></span></p> <br />
+      <p>Total bookings = <span></span></p><br />
+      <p>Total Facility = <span></span></p><br />
+      <p>Total Facility Requests = <span></span></p><br />
+
+      <button>Add admins</button><br />
+      <button>View Facilitiy request</button><br />
+      <button>Manage Facility</button><br />
+      <button>View users</button><br />
     </div>
   );
 }
