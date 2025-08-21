e7ca236afa667f7e99d72f7eb9784d859bbb4b19
Kartikeya <kartikeya.anjul@gmail.com>
2025-08-21 21:03:45 +0530

    added owner dashboard

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -15,6 +15,8 @@ function Home() {
     };
   };
 
+
+
   return (
     <div>
       {isloggedIn ? (
--- src/pages/owner.jsx
+++ src/pages/owner.jsx
@@ -1,11 +1,11 @@
 import React from "react";
+import { useState } from "react";
+
 
 const Owner = () => {
   return (
     <div>
-      <h1>Owner Page</h1>
-      <p>Welcome to the owner page!</p>
-      <p>Here you can manage the application settings and user accounts.</p>
+      
     </div>
   );
 }
