ad8415632a9c9e5b4fb8f0358c0cd7e8bbd3799b
Kartikeya <kartikeya.anjul@gmail.com>
2025-08-23 23:49:27 +0530

    error correction and admin page structure

--- src/pages/admin.jsx
+++ src/pages/admin.jsx
@@ -7,7 +7,7 @@ const Admin = () => {
 
   useEffect(() => {
     const storedRole = localStorage.getItem("role");
-    setRole(storedRole ? JSON.parse(storedRole) : null);
+    setRole(storedRole ? storedRole : null);
   }, []);
 
   useEffect(() => {
@@ -21,8 +21,14 @@ const Admin = () => {
   }
 
   return (
-  <h1>Admin</h1>
-);
+        <>
+            <h1>Admin</h1>
+            <p>Your facility (total = <span></span>)</p>  {/*if admin click on one of the facility then all information and booking details must be shown*/}
+            <div></div> {/*show all the facility which are aproved by the owner*/}
+            <button>Add new Facility</button> <br />
+            <button>check facility approve status</button>
+        </>
+    );
 };
 
 export default Admin;
