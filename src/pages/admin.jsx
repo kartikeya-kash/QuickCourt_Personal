fdfb7706a18390b04dd924361c756884c66683cb
Kartikeya <kartikeya.anjul@gmail.com>
2025-08-24 12:30:58 +0530

    added add facility

--- src/pages/admin.jsx
+++ src/pages/admin.jsx
@@ -8,8 +8,9 @@ const Admin = () => {
     const location = useLocation();
     const { username, password } = location.state || {};
     localStorage.setItem("adminusername", username);
-    const adminusername = localStorage.getItem("username");
+    const adminusernamefromlocal = localStorage.getItem("adminusername");
     const [role, setRole] = useState(undefined); 
+    const [addfacilitypop, setAddFacilitypop] = useState(false);
 
   useEffect(() => {
     const storedRole = localStorage.getItem("role");
@@ -33,14 +34,93 @@ const logout = () => {
   navigate("/");
 };
 
+const addnewfacility = () => {
+  setAddFacilitypop(true);
+};
+
   return (
         <>
-            <h1>Admin name= {adminusername}</h1>
+            <h1>Admin name= {adminusernamefromlocal}</h1>
             <p>Your facility (<span></span>)</p>  {/*if admin click on one of the facility then all information and booking details must be shown*/}
             <div></div> {/*show all the facility which are aproved by the owner*/}
-            <button>Add new Facility</button> <br />
+            <button onClick={addnewfacility}>Add new Facility</button> <br />
             <button>check facility approve status</button> <br />
             <button onClick={logout}>logout</button>
+
+       {addfacilitypop && (
+        <div style={{
+        position: "fixed",
+        top: 0, left: 0,
+        width: "100%", height: "100%",
+        background: "rgba(0, 0, 0, 0.76)",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+        }}>
+    <div style={{ background: "rgba(72, 70, 70, 1)", padding: "20px", minWidth: "300px" }}>
+      <h2 style={{ textAlign: "center" }} >Add new facility</h2>
+      <form style={{ textAlign: "center" }}>
+        <div>
+          <label>Facility ID:</label>
+          <input type="text" name="name" required/>
+        </div>
+        <div>
+          <label>Facility Name:</label>
+          <input type="text" name="name" required/>
+        </div>
+        <div>
+          <label>Facility email:</label>
+          <input type="text" name="name" required/>
+        </div>
+        <div>
+          <label>Facility location:</label>
+          <input type="text" name="name" required/>
+        </div>
+        <div>
+          <label>Facility images:</label>
+          <input type="file" name="name" required/>
+        </div>
+
+        <div>
+          <label>Facility sport:</label><br />
+          <input type="checkbox" name="name" required/> Football 
+          <input type="checkbox" name="name" required/> Cricket
+          <input type="checkbox" name="name" required/> Badminton
+          <input type="checkbox" name="name" required/> Tennis<br />
+          <input type="checkbox" name="name" required/> Swimming
+          <input type="checkbox" name="name" required/> Gym
+          <input type="checkbox" name="name" required/> Yoga
+          <input type="checkbox" name="name" required/> basketball<br />
+          <input type="checkbox" name="name" required/> Volleyball
+          <input type="checkbox" name="name" required/> Table Tennis
+          <input type="checkbox" name="name" required/> Squash
+          <input type="checkbox" name="name" required/> Athletics <br />
+          <input type="checkbox" name="name" required/> Cycling
+          <input type="checkbox" name="name" required/> sumoing
+          <input type="checkbox" name="name" required/> Hiking
+          <input type="checkbox" name="name" required/> Fishing<br />
+          <input type="checkbox" name="name" required/> Climbing
+          <input type="checkbox" name="name" required/> Surfing
+          <input type="checkbox" name="name" required/> Skiing
+          <input type="checkbox" name="name" required/> Snowboarding<br />
+          <input type="checkbox" name="name" required/> Skating
+          <input type="checkbox" name="name" required/> Martial Arts
+          <input type="checkbox" name="name" required/> Dance
+          <input type="checkbox" name="name" required/> Gymnastics<br />
+          <input type="checkbox" name="name" required/> carting
+          <input type="checkbox" name="name" required/> others
+        </div>
+
+        <div style={{ marginTop: "10px" }}>
+          <button type="submit" >Add Facility</button>
+        </div>
+        <div style={{ marginTop: "10px" }}>
+          <button type="button" onClick={() => setAddFacilitypop(false)}>Close</button>
+        </div>
+      </form>
+    </div>
+  </div>
+)}
         </>
     );
 };
