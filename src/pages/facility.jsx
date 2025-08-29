dd7841fb3f58f7a98803b6bfe8ee6aacdad87fea
Kartikeya <kartikeya.anjul@gmail.com>
2025-08-29 22:18:57 +0530

    added cards component

--- src/pages/facility.jsx
+++ src/pages/facility.jsx
@@ -1,6 +1,7 @@
 import React from "react";
+import Card from "./components/facilitycard";
 
 const Facility = () => {
-    return <div>Facility Page</div>
-}
+  return <Card name="abcd" location="xyz" />;
+};
 export default Facility;
