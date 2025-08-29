ab0c5361050f9eb834ba771817c8264047c83c35
Kartikeya <kartikeya.anjul@gmail.com>
2025-08-29 23:11:43 +0530

    added navbar and heading

--- src/pages/facility.jsx
+++ src/pages/facility.jsx
@@ -2,17 +2,26 @@ import React, { useState, useEffect } from "react";
 import Card from "./components/facilitycard";
 import Loader from "./components/loader";
 import styled from "styled-components";
+import Button from "./components/button";
+import { useNavigate } from "react-router-dom";
 
 const Facility = () => {
+  const navigate = useNavigate();
   const [Facilitydata, setFacilitydata] = useState([]);
   const [loading, setLoading] = useState(true);
+
+  const logout = () => {
+    localStorage.removeItem("isloggedin");
+    localStorage.removeItem("role");
+    localStorage.removeItem("username");
+    navigate("/");
+  };
 
   useEffect(() => {
     fetch("http://localhost:5005/facilitylistforusers")
       .then((res) => res.json())
       .then((data) => {
         setFacilitydata(data);
-        console.log("✅ Fetched facilities:", data);
         setLoading(false);
       })
       .catch((err) => {
@@ -27,6 +36,13 @@ const Facility = () => {
 
   return (
     <>
+      <div style={{ display: "flex", gap: "10px", marginTop: "15px" }}>
+        <Button text="Home" onClick={() => navigate("/")} />
+        <Button text="Facilities" onClick={() => navigate("/facility")} />
+        <Button text="My Bookings" onClick={() => navigate("/mybookings")} />
+        <Button text="Me" onClick={() => navigate("/me")} />
+        <Button text="Logout" onClick={logout} color="red" />
+      </div>
       <h1 style={{ textAlign: "center" }}>Facility Page</h1>
       <GridWrapper>
         {Facilitydata.map((facility) => (
